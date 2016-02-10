1dddda86955549f82fff81093a84332d07bb90ba
Rafael Salerno <salerno.rafael@gmail.com>
2016-02-10 09:58:26 -0200

    changed sample

--- sample-ten/public/js/app.js
+++ sample-ten/public/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-	var app = angular.module('store',['panel-module']); 
+	var app = angular.module('store',[]);
 
 	app.controller('StoreController',['$http', function($http){
 		var store =this;
@@ -8,6 +8,43 @@
 			
 			store.products = data;
 		});
-	}]);	
+	}]);
+
+	app.directive('productHeader',function(){
+		return{
+			restrict:'E',
+			templateUrl:'product-header.html'
+		};
+	});
+
+	app.directive('productHeaderAttribute',function(){
+		return{
+			restrict:'A',
+			templateUrl:'product-header-attribute.html'
+		};
+	});
+
+	app.controller('CommentController', function(){
+		this.comment ={};
+		
+		this.addComment = function(product){
+			product.comments.push(this.comment);
+			this.comment ={};
+		};
+
+	});
+
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
 
 })();
